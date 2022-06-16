34fca0cb38c580daeae0a62a2eaa327572edf5c7
BenardAbwao <benard.okoth.abwao@gmail.com>
2022-06-16 15:00:06 +0300

    commit tree fd771dc22ee8d961c7e7066eda7804651b4bd7bf

---
+++ js/challenge.js
@@ -0,0 +1,106 @@
+let counter = document.querySelector('#counter')
+let counterValue = parseInt(counter.innerHTML)
+
+
+
+function counterFunc(){
+    counter.innerHTML = counterValue += 1;
+}
+
+let intervals = setInterval(counterFunc, 1000)
+
+
+// Manually add counter 
+let add = document.querySelector("#plus")
+let subtract = document.querySelector("#minus")
+let heartbtn = document.querySelector("#heart")
+let pausebtn = document.querySelector("#pause")
+let resetbtn = document.querySelector("#reset")
+
+add.addEventListener("click", addManually)
+subtract.addEventListener('click', subtractManualy)
+pausebtn.addEventListener('click', btnCall);
+
+function addManually(){
+    counter.innerHTML = counterValue +=1;
+}
+
+function subtractManualy(){
+    counter.innerHTML = counterValue -= 1;
+}
+
+
+let anArray = []
+let counter2  = 0;
+let setTime = 0;
+let index = -1
+
+
+function callCounter(){
+    if(setTime === 0){
+        setTime = counterValue;
+        counter2 += 1
+    }else if( counterValue !== setTime){
+
+        likesSet()
+
+        setTime = 0;
+        anArray = [];
+        counter2 = 0;
+    } else if(counterValue === setTime){
+        anArray.push(counter2 += 1)
+    }
+    
+}
+function likesSet(){
+    let likes = document.querySelector('.likes')
+    let li = document.createElement('li')
+    li.innerHTML = `${setTime} has been liked ${anArray[anArray.length - 1]} time`
+    likes.appendChild(li)
+}
+
+heartbtn.addEventListener('click', callCounter)
+ if(parseInt(counter) === counterValue){
+     heartbtn.addEventListener("click", callCounter)
+     console.log(anArray)
+ } else if(counter === counterValue - 1) {
+     console.log(anArray[anArray.length-1])
+ }
+
+//comments
+
+let myForm = document.querySelector("form");
+myForm.addEventListener("submit", getComment)
+let myInput = document.querySelector("#comment-input");
+let comments = document.querySelector('#list')
+
+function getComment(e){
+    e.preventDefault()
+    let p = document.createElement('p');
+    p.innerHTML = `${myInput.value}`
+    comments.appendChild(p)
+    myInput.value = "";
+}
+
+function btnCall(){
+    if(pausebtn.innerText === "pause"){
+        ResetIntervals()
+        add.setAttribute('disabled', "")
+        subtract.setAttribute('disabled', "")
+        heartbtn.setAttribute('disabled', "")
+        pausebtn.innerHTML = 'resume'
+    }
+    else if(pausebtn.innerHTML === 'resume'){
+        ResetIntervals()
+        add.removeAttribute('disabled', "")
+        subtract.removeAttribute('disabled', "")
+        heartbtn.removeAttribute('disabled', "")
+        pausebtn.innerHTML = 'pause'
+        intervals = setInterval(counterFunc, 1000)
+        
+    }
+}
+
+function ResetIntervals(){
+    clearInterval(intervals)
+}
